ba8a252388d8b7ead265c664a67b08c478681714
EricDar12 <eric123724@gmail.com>
2025-12-09 17:47:53 -0500

    Implemented bootstrap and basic styling

--- Tabula/Models/Account/RegisterViewModel.cs
+++ Tabula/Models/Account/RegisterViewModel.cs
@@ -6,9 +6,11 @@ namespace Tabula.Models.Account
     {
         [Required(ErrorMessage = "Email is required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Display(Name = "Email:")]
         public string Email { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username:")]
         public string Username { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Password is required")]
@@ -16,9 +18,11 @@ namespace Tabula.Models.Account
         [StringLength(100, MinimumLength = 8, ErrorMessage = "Password must be at least 8 characters long")]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*\W).+$",
             ErrorMessage = "Password must contain at least one uppercase letter, one number, and one special character")]
+        [Display(Name = "Password:")]
         public string Password { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Confirm your password")]
+        [Display(Name = "Confirm Password:")]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Passwords do not match")]
         public string ConfirmPassword { get; set; } = string.Empty;
