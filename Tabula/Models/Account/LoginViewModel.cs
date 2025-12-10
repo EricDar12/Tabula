f325dce2171c2b7d434de912511a67eddf473064
EricDar12 <eric123724@gmail.com>
2025-12-10 18:13:53 -0500

    Basic styling improvements

--- Tabula/Models/Account/LoginViewModel.cs
+++ Tabula/Models/Account/LoginViewModel.cs
@@ -6,12 +6,15 @@ namespace Tabula.Models.Account
     {
         [Required(ErrorMessage = "Email is Required")]
         [EmailAddress(ErrorMessage = "Invalid email address")]
+        [Display(Name = "Email:")]
         public string Email { get; set; } = string.Empty;
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password:")]
         public string Password { get; set; } = string.Empty;
 
+        [Display(Name = "Stay signed in")]
         public bool RememberMe { get; set; } = false;
     }
 }
